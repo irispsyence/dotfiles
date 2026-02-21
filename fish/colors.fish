3120cd7923ca94a21c5f22f44906cb41b11e8e5f
irispsyence <irispsyence@gmail.com>
2026-02-20 21:37:24 -0500

    initial dotfiles commit

---
+++ fish/colors.fish
@@ -0,0 +1,3 @@
+set -g fish_color_primary 88d6ba
+set -g fish_color_secondary b2ccc1
+set -g fish_color_tertiary a7cce1
---
+++ matugen/templates/fish-colors.fish
@@ -0,0 +1,3 @@
+set -g fish_color_primary {{colors.primary.default.hex_stripped}}
+set -g fish_color_secondary {{colors.secondary.default.hex_stripped}}
+set -g fish_color_tertiary {{colors.tertiary.default.hex_stripped}}
