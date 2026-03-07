f938f5796a71e56bf2f348fafcacd94f8c0b6c76
irispsyence <irispsyence@gmail.com>
2026-03-07 14:54:26 -0500

    sync fish and ghostty configs from live machine

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 	source ~/.config/fish/colors.fish
 end
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
